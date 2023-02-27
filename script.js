c08497ae591e303c7bbd47a8e35fe294a9e99a25
Arthi A <arthianbalagan180@gmail.com>
2023-02-27 16:45:39 +0530

    completing multiple product card

---
+++ script.js
@@ -0,0 +1,61 @@
+
+function myFunctionblack() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("imgchair").src="./images/black chair.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '450px';
+    // image.style.height = '850px';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to right, #232526, #414345)";
+    document.getElementById("content-box").style.color="white";
+}
+function myFunctionblue() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("imgchair").src="./images/blue chair.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '450px';
+    // image.style.height = 'auto';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to right, #000428, #004e92)";
+    document.getElementById("content-box").style.color="white";
+}
+function myFunctiongreen() {
+    document.body.style.backgroundColor = "green";
+    document.getElementById("imgchair").src="./images/green chain.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '500px';
+    // image.style.height = '850px';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to right, #000000, #0f9b0f)";
+    document.getElementById("content-box").style.color="white";
+}
+function myFunctionorange() {
+    document.body.style.backgroundColor = "orange";
+    document.getElementById("imgchair").src="./images/orange chair.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '450px';
+    // image.style.height = '850px';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to right, #ff8008, #ffc837)";
+    document.getElementById("content-box").style.color="white";
+}
+function myFunctionred() {
+    document.body.style.backgroundColor = "red";
+    document.getElementById("imgchair").src="./images/red chair.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '450px';
+    // image.style.height = '850px';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to left, #333333, #dd1818)";
+    document.getElementById("content-box").style.color="white";
+}
+function myFunctionyellow() {
+    document.body.style.backgroundColor = "yellow";
+    document.getElementById("imgchair").src="./images/yellow chair.png";
+    var image = document.getElementById('imgchair');
+    // image.style.width = '450px';
+    // image.style.height = '850px';
+    document.getElementById("content-box").style.backgroundImage="linear-gradient(to left, #fffc00, #ffffff)";
+    document.getElementById("content-box").style.color="black";
+}
+function myFunctioncart(){
+    alert("Added to the card");
+}
+function myFunctionbuy(){
+    alert("Good choice");
+}
